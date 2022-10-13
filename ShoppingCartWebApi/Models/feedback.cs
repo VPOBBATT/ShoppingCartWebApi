2346f93924c69d3a69152286cb05a6da5c671853
pobbatti vijaya lakshmi <honeyvijji643@gmail.com>
2022-10-13 19:33:32 +0530

    Add project files.

---
+++ ShoppingCartWebApi/Models/feedback.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoppingCartWebApi.Models
+{
+    public class feedback
+    {
+        [Key]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Feedback { get; set; }
+
+        public virtual UserDetails UserDetails { get; set; }
+    }
+}
